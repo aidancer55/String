579cf0780e4baa2f42cb86f40a31e86ebf9ddfa6
aidancer55 <bluebonnets77@gmail.com>
2022-01-26 22:40:18 +0300

    Добавьте файлы проекта.

---
+++ 2.Palindrom/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            char[] reverse = str.ToCharArray();
+            Array.Reverse(reverse);
+            string nstr = new string(reverse);
+            int result = string.CompareOrdinal(str, nstr);
+            if (result == 0)
+            {
+                Console.WriteLine("Палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Не палиндром");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] array = s.Split();
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length > a)
+                {
+                    a = array[i].Length;
+                    b = i;
+                }
+            }
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
